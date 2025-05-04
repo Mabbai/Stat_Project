0292e0e0d66e1ad513104dc80df37eef26f631c9
Mabbai <127123695+Mabbai@users.noreply.github.com>
2025-05-04 22:38:08 +0200

    Update perculation.py

--- perculation.py
+++ perculation.py
@@ -1,52 +1,55 @@
 import random as rn
-from collections import defaultdict
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
-graph_size = 30
+
+GRAPH_SIZE = 1000
+EDGE_PROB = 0.501
+
+        
+def EXIST_EDGE(p):
+  return rn.random() < p
+
 def generate_graph():
   G = {}
-  n = graph_size
-  p = 0.50
+  n = GRAPH_SIZE
   for i in range(0,n+1):
     for j in range(0,n+1):
       G[f'{i},{j}'] = []
   for i in range(0,n+1):
     for j in range(0,n+1,2):
-      if i & 1: # if i modulo 4 is 1
+      if i & 1: # i modulo 2 = 1
         j += 1
       if j == n+1:
         continue
       Domestic_Edges = []
 
-      if EXIST_EDGE(p) and j+1 <= n:
+      if EXIST_EDGE(EDGE_PROB) and j+1 <= n:
         Domestic_Edges.append(f'{i},{j+1}')
         
         G[f'{i},{j+1}'].append(f'{i},{j}')
 
-      if EXIST_EDGE(p) and i+1 <= n:
+      if EXIST_EDGE(EDGE_PROB) and i+1 <= n:
         Domestic_Edges.append(f'{i+1},{j}')
         
         G[f'{i+1},{j}'].append(f'{i},{j}') 
-      if EXIST_EDGE(p) and i-1 >= 0:
+      if EXIST_EDGE(EDGE_PROB) and i-1 >= 0:
         Domestic_Edges.append(f'{i-1},{j}')
         
         G[f'{i-1},{j}'].append(f'{i},{j}')
       
-      if EXIST_EDGE(p) and j-1 >= 0:
+      if EXIST_EDGE(EDGE_PROB) and j-1 >= 0:
         Domestic_Edges.append(f'{i},{j-1}')
         
         G[f'{i},{j-1}'].append(f'{i},{j}')
+
       G[f'{i},{j}'] = Domestic_Edges
-  
   return G
-        
-def EXIST_EDGE(p):
-  return rn.random() < p
+
 
 G = generate_graph()
-print(f'{G}+\n')
+#print(f'{G} \n')
 
 def draw_graph_from_dict(grid) -> None:
   G = nx.Graph()
@@ -61,29 +64,47 @@ def draw_graph_from_dict(grid) -> None:
   plt.gca().invert_yaxis()
   plt.show()
 
-total_nodes_checked = []
 
-def graph_explorer(node, path_trekked):
-  #print(node)
-  if int(node.split(',')[0]) == graph_size:
-    print(f'WIN! {path_trekked}')
-    return 'WIN'
+searched_starts = set()
+
+def iterate_frontier(start):
+  old_frontier = set()
+  frontier = {start}
+  while frontier:
+    leafs = set()
+    for node in frontier:
+      acc = grow_leafs(node, frontier | old_frontier)
+      if acc == 'W':
+        print('W')
+        return 'W'
+      leafs = leafs | acc
+      
+    print(leafs)
+    old_frontier = frontier
+    frontier = leafs
+    
+        
+def grow_leafs(node, past_nodes):
+  leafs = set()
   for n in G[node]:
-    if n in path_trekked:
-      continue
-    if n in total_nodes_checked:
-      continue
-    
-    total_nodes_checked.append(n)
-    if graph_explorer(n, path_trekked + [n]) == 'WIN':
-      return 'WIN'
+    if not n in past_nodes | leafs:
+      x_val = int(n.split(',')[0])
+      if x_val == GRAPH_SIZE:
+        return 'W'
+      
+      if x_val == 0:
+        searched_starts.add(n)
+      leafs.add(n)
 
-for i in range(0,graph_size+1):
+  return leafs
+
+i=0
+RESULT = 'L'
+while i <= GRAPH_SIZE and RESULT != 'W':
   node = f'0,{i}'
-  if node in total_nodes_checked:
-    continue
-  total_nodes_checked.append(node)
-  graph_explorer(node, [node])
-    
+  if not node in searched_starts:
+    RESULT = iterate_frontier(node)
+  i += 1
 
-draw_graph_from_dict(G)
+#draw_graph_from_dict(G) //don't run it for large graphs 15<
+
